fba3014f0c133d3722c6cc7ac9e58fa1b1366241
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-09-19 18:03:24 +0200

    Tested logout component also checking local storage with token

--- src/components/LogoutControl/LogoutControl.test.tsx
+++ src/components/LogoutControl/LogoutControl.test.tsx
@@ -11,9 +11,11 @@ jest.mock("react-router-dom", () => ({
 
 let mockLogged = true;
 
-describe("Given a LoginControl component", () => {
-  describe("When it's invoked and logged status is true", () => {
+describe("Given a LogoutControl component", () => {
+  describe("When it's invoked and logged status is true and there is a token", () => {
     test("Then it should call navigate with bookings page path", () => {
+      const tokenExists = "token exists";
+      Storage.prototype.getItem = jest.fn(() => tokenExists);
       const loginPagePath = "/bookings";
       const mockUserSlice = createSlice({
         name: "user",
@@ -34,6 +36,34 @@ describe("Given a LoginControl component", () => {
       );
 
       expect(mockUseNavigate).toHaveBeenCalledWith(loginPagePath);
+    });
+  });
+
+  describe("When it's invoked and logged status is true but there is no token", () => {
+    test("Then it should render a level 1 heading with text 'Children'", () => {
+      const noToken = "";
+      Storage.prototype.getItem = jest.fn(() => noToken);
+      const mockUserSlice = createSlice({
+        name: "user",
+        initialState: { logged: mockLogged },
+        reducers: {},
+      });
+
+      const mockStore = configureStore({
+        reducer: { user: mockUserSlice.reducer },
+      });
+
+      render(
+        <Provider store={mockStore}>
+          <LogoutControl>
+            <h1>Children</h1>
+          </LogoutControl>
+        </Provider>
+      );
+
+      const childrenText = screen.getByRole("heading", { name: "Children" });
+
+      expect(childrenText).toBeInTheDocument();
     });
   });
 
